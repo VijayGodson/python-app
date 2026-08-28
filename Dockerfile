27338a81cb8fce54986c8161d039f3f2b73dd0c0
Vijay <Vijay@gmail.com>
2026-08-28 00:02:48 -0400

    Adds python-app application with k8s delpoyments

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+ FROM python:3.14.7-alpine
+
+ COPY requirements.txt /tmp
+
+ RUN pip3 install -r /tmp/requirements.txt
+
+ COPY src /src
+
+ CMD python /src/app.py
---
+++ src/app.py
@@ -0,0 +1,36 @@
+from flask import Flask,jsonify
+import datetime, os, time
+import socket
+
+app = Flask(__name__)
+
+app.json.compact = False
+app.json.sort_keys = False
+
+CONTAINER_START_TIME = os.path.getmtime('/proc/1')
+
+@app.route('/api/v1/details')
+
+def details():
+    uptime_seconds = int(time.time() - CONTAINER_START_TIME)
+    uptime_string = str(datetime.timedelta(seconds=uptime_seconds))
+
+    return jsonify({
+        'local_now' : datetime.datetime.now().astimezone(),
+        'hostname': socket.gethostname(),
+        'uptime': uptime_string
+    })
+
+@app.route('/api/v1/healthz')
+
+def health():
+    return jsonify({
+        'status': 'up'
+        }), 200
+
+if __name__ == '__main__':
+
+    app.run(host="0.0.0.0")
+
+#'/api/v1/details'
+#'/api/v1/healthz'
